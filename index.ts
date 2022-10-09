79677f889bd6bae6b94908f1667b7e308ed20e09
Khoa Ngo <nmkhoa17ck1@gmail.com>
2022-10-09 16:06:24 +0700

    Issue#3 - Initial Database

--- index.ts
+++ index.ts
@@ -3,10 +3,12 @@ import './src/application/config';
 import { defaultLogger } from '@gln-libs/node-infrastructure';
 
 import { startHttpServer } from './src/application/server';
+import { connectMongoDB } from './src/datasource/mongodb/datasource';
 
 global.logger = defaultLogger();
 
 const start = async () => {
+  await connectMongoDB();
   startHttpServer();
 };
 
